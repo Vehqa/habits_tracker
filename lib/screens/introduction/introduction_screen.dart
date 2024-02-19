d36827a740125807f7d2d628f8590e3f20dd9f30
Vehqa <pro.nectoux@gmail.com>
2024-02-19 18:09:18 +0100

    Intro screens template

--- lib/screens/introduction/introduction_screen.dart
+++ lib/screens/introduction/introduction_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 class IntroductionScreen extends StatefulWidget {
   const IntroductionScreen({super.key});
@@ -8,20 +9,83 @@ class IntroductionScreen extends StatefulWidget {
 }
 
 class _IntroductionScreenState extends State<IntroductionScreen> {
+  final PageController _pageController = PageController();
+  bool lastPage = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: PageView(children: [
-        Container(
-          color: Colors.amber,
-        ),
-        Container(
-          color: Colors.red,
-        ),
-        Container(
-          color: Colors.blue,
-        ),
-      ]),
+      body: Stack(
+        children: [
+          PageView(
+            onPageChanged: (index) {
+              setState(() {
+                lastPage = (index == 2);
+              });
+            },
+            controller: _pageController,
+            children: [
+              Container(
+                color: Colors.amber,
+              ),
+              Container(
+                color: Colors.red,
+              ),
+              Container(
+                color: Colors.blue,
+              ),
+            ],
+          ),
+          Container(
+            alignment: const Alignment(0, 0.75),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    _pageController.animateToPage(2,
+                        duration: const Duration(milliseconds: 600),
+                        curve: Curves.linear);
+                  },
+                  child: const Text(
+                    'Skip',
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                  ),
+                ),
+                SmoothPageIndicator(controller: _pageController, count: 3),
+                lastPage
+                    ? GestureDetector(
+                        onTap: () {
+                          _pageController.nextPage(
+                              duration: const Duration(milliseconds: 600),
+                              curve: Curves.linear);
+                        },
+                        child: const Text(
+                          'Done',
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      )
+                    : GestureDetector(
+                        onTap: () {
+                          _pageController.nextPage(
+                              duration: const Duration(milliseconds: 600),
+                              curve: Curves.linear);
+                        },
+                        child: const Text(
+                          'Next',
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
